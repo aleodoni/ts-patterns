a93c8c14f67d3d9f28711a4af4808cab4330d472
aleodoni <aleodoni@gmail.com>
2021-02-09 09:48:53 -0300

    infra: remove build directory and update .gitignore

--- build/decorator/CoffeeDecorator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var CoffeeDecorator = /** @class */ (function () {
-    function CoffeeDecorator() {
-    }
-    return CoffeeDecorator;
-}());
-exports.default = CoffeeDecorator;
--- build/decorator/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var Espresso_1 = __importDefault(require("./beverages/Espresso"));
-var coffee = new Espresso_1.default();
-console.log(coffee.getDescription());
-console.log(coffee.cost());
